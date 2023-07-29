bcf41096117b1bc46e0ad97b54edc16c99f21397
Zourunfa <473023989@qq.com>
2023-07-29 19:19:47 +0800

    feat: base study

--- src/practice_for_base/mod.rs
+++ src/practice_for_base/mod.rs
@@ -57,6 +57,82 @@ fn biBaoMove() {
     // return y;
 }
 
+// impl 关键字
+/**
+ *
+在Rust中，impl 关键字是实现对象行为的主要机制。
+你可以使用它来定义和实现结构体(struct)、枚举(enum)或者特性(trait)的方法。
+ */
+
+// impl作用1：结构体和枚举的方法实现，可以使用impl为结构体或枚举定义方法
+struct Point {
+    x: f64,
+    y: f64,
+}
+
+impl Point {
+    fn new(x: f64, y: f64) -> Point {
+        Point { x, y }
+    }
+
+    fn distance(&self) -> f64 {
+        (self.x.powf(2) + self.y.powf(2)).sqrt()
+    }
+}
+
+// 特性实现:用impl关键字为特定类型实现trait
+trait Drawable {
+    fn draw(&self);
+}
+
+struct Circle {
+    radius: i32,
+}
+
+impl Drawable for Circle {
+    fn draw(&self) {
+        // 绘制一个圆
+    }
+}
+
+
+// 关联函数：使用impl定义一种不需要实例就可以调用的方法
+// 类似与js的静态方法
+
+struct Point {
+  x: i32,
+  y: i32,
+}
+
+impl Point {
+  // 这就是一个关联函数
+  fn origin() -> Point {
+      Point { x: 0, y: 0 }
+  }
+}
+
+// 使用关联函数
+let origin = Point::origin();
+
+
+//默认方法 如果你在特性定义中使用 impl，你可以为方法提供默认的实现。
+// 实现该特性的类型可以选择覆盖这个默认实现
+trait Drawable {
+  fn draw(&self) {
+      // 提供默认的绘制实现
+  }
+}
+
+struct Circle {
+  radius: i32,
+}
+
+// 因为Drawable有默认实现，所以这里可以什么都不写
+impl Drawable for Circle {}
+
+
+
+
 #[test]
 
 fn average_score_test() {
