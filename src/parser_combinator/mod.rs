6813244a90ce565c47a33dd8c2b04f0e7baf3d40
Zourunfa <473023989@qq.com>
2023-08-02 20:37:21 +0800

    feat: practice

--- src/parser_combinator/mod.rs
+++ src/parser_combinator/mod.rs
@@ -52,7 +52,104 @@ impl std::error::Error for ParseErr {}
 
 type ParseResult = Result<(String, ParseObj), ParseErr>;
 
+fn parse_char(c: char) -> impl Fn(String) -> ParseResult {
+    return move |input: String| {
+        if input.is_empty() {
+            return ParseResult::Err(ParseErr::Unexpected(
+                c.to_string(),
+                "nothing".to_string(),
+                0,
+            ));
+        }
+
+        if input.chars().nth(0).unwrap() == c {
+            return ParseResult::Ok((input[1..].to_string(), ParseObj::Char(c)));
+        }
+
+        return ParseResult::Err(ParseErr::Unexpected(
+            c.to_string(),
+            input.chars().nth(0).unwrap().to_string(),
+            0,
+        ));
+    };
+}
+
+fn any_of(parsers: Vec<impl Fn(String) -> ParseResult>) -> impl Fn(String) -> ParseResult {
+    return move |input: String| {
+        for parser in parsers.iter() {
+            match parser(input.clone()) {
+                Ok((remains, parserd)) => return Ok((remains, parserd)),
+                Err(err) => continue,
+            }
+        }
+
+        return ParseResult::Err(ParseErr::Unexpected(
+            input.to_string(),
+            "nothing".to_string(),
+            0,
+        ));
+    };
+}
+
+fn zero_or_more(parser: impl Fn(String) -> ParseResult) -> impl Fn(String) -> ParseResult {
+    return move |mut input: String| {
+        let mut result = Vec::new();
+        while let Ok((remains, parserd)) = parser(input.clone()) {
+            input = remains;
+            result.push(parserd);
+        }
+
+        return Ok((input.clone(), ParseObj::List(result)));
+    };
+}
+
+fn any_whitespace() -> impl Fn(String) -> ParseResult {
+    let sp = parse_char(' ');
+    let tab = parse_char('\t');
+    let newline = parse_char('\n');
+
+    return any_of(vec![sp, tab, newline]);
+}
+
+fn parse_chars(input: &str) -> impl Fn(String) -> ParseResult {
+    let parsers = chars.chars().map(|c| parse_char(c)).collect();
+    return any_of(parsers);
+}
+
+fn one_or_more(parser: impl Fn(String) -> ParseResult) ->impl Fn(String) -> ParseResult {{
+  
+}
+
+fn ident(input: String) -> ParseResult {
+    match one_or_more(parse_char(
+        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ_",
+    ))(input) {}
+}
+
+fn whitespace() -> impl Fn(String) -> ParseResult {
+    return zero_or_more(any_whitespace());
+}
+
+fn decl(mut input: String) {
+    let (remains, _) = whitespace()(input.clone()).unwrap();
+    println!("whitespace remains{:?}", remains);
+
+    let (remains, obj) = ident(remains).unwrap();
+    let mut identifier = "".to_string();
+
+    match obj {
+        ParseObj::Ident(i) => identifier = i,
+        _ => {
+            // return Err(ParseErr::Unexpected(
+            //     "ident".to_string(),
+            //     format!("{:?}", obj),
+            //     0,
+            // ))
+        }
+    }
+    println!("ident: {} remains: \"{}\"", identifier, remains);
+}
 #[test]
 fn test_parse_decl_bool() {
-    let decl_res = decl("a = false".to_string());
+    let decl_res = decl("  a = false".to_string());
 }
