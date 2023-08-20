c0b50e97356968fa2929135b71cc200e2955a35f
Zourunfa <473023989@qq.com>
2023-08-20 18:39:45 +0800

    feat:base study

--- src/practice_for_base/mod.rs
+++ src/practice_for_base/mod.rs
@@ -167,6 +167,32 @@ fn divide(numerator: i32, denominator: i32) -> Option<f64> {
     }
 }
 
+// 包和 crate
+/*
+crate是一个二进制的库，crate root是一个源文件，rust编译器已他为起始点，并构成你的
+crate根模块，包是提供一系列功能的一个或者多个crate,一个包会包含一个cargo.html文件，
+阐述如何去构件这些crate
+
+包中所包含的内容由几条规则来确立，一个包中至多只能包含一个库，包中可以包含多个crate库，包
+中至少包含一个crate,无论是库还是二进制的
+
+cargo new my-project
+
+当我们输入了这条命令，
+Cargo 会给我们的包创建一个 Cargo.toml 文件。查看 Cargo.toml 的内容，
+会发现并没有提到 src/main.rs，因为 Cargo 遵循的一个约定：src/main.rs
+ 就是一个与包同名的二进制 crate 的 crate 根。同样的，Cargo 知道如果包目录中包含
+ src/lib.rs，则包带有与其同名的库 crate，
+且 src/lib.rs 是 crate 根。crate 根文件将由 Cargo 传递给 rustc 来实际构建库或者二进制项目。
+
+
+在此，我们有了一个只包含 src/main.rs 的包，
+意味着它只含有一个名为 my-project 的二进制 crate。
+如果一个包同时含有 src/main.rs 和 src/lib.rs，则它有两个 crate：
+一个库和一个二进制项，且名字都与包相同。通过将文件放在 src/bin 目录下，
+一个包可以拥有多个二进制 crate：每个 src/bin 下的文件都会被编译成一个独立的二进制 crate。
+*/
+
 #[test]
 fn Result_Options_test() {
     match open_file("non_existent_file.txt") {
